94ae00c754556949100321f5f323dc44f55bb21b
jenifer0 <jenifer.vizcarra.0@gmail.com>
2018-01-19 00:52:28 -0500

    3

--- index.php
+++ index.php
@@ -3,3 +3,50 @@ html>
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 	<?php include('header.html');?>
 </head>
+<body>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos!</h1>
+			</div>
+
+			<div class="articulo_ini">
+					<br/>
+				<center><a href="#"><img src="img/local1.jpg" WIDTH=500 alt="local"></a></center>
+				<article ><br/><br/>
+					<p>Picanteros Arequipeños ofrece al cliente nacional como extranjero, una gran
+						variedad de los mejores platos tipicos de la renombrada comida arquipeña.
+						Sus Piqueos y Sabores, han cautivado a cada visitante; por lo que contamos
+						con una clientela cautiva, que siempre regresa.</p>
+					<br/>
+					<p>Nuestra presencia en Mistura, nos permite competir y reconocer que vamos
+					por buen camino, es un orgullo para nosotros representar a nuestra tierra, Arequipa
+					en un evento de cocina tan importanto, como es esta feria gastronómica.</p>
+					<p>Pero no solo ofrecemos servicio de restaurante, nuestras instalaciones son amplias y
+						muy elegantes, por lo que tambien realizamos eventos, desde cumpleaños hasta
+						ceremonias de matrimonios. Si desea visitarnos para conmemorar alguna fecha especial
+						no dude en comunicarse con nosotros en nuestro link de RESERVAS e indicar en la
+						descripción los detalles más importantes sobre el evento que desea realizar, nosotros
+						nos comunicaremos con usted a la brevedad posible, tambien puede comunicarse a nuestros
+						telefonos, estaremos gustosos de atenderlos.
+					<br/><br/><br/>
+
+					<p>Atendemos de martes a domingo en Av. Fernandini 1234 - Sachaca
+					</p>
+
+
+				</article>
+				<br/><br/>
+				<center><?php include('cookies2.php');?><center>
+			</div>
+
+
+
+
+		</div>
+	</section>
+
+<?php include('footer.html');?>
+
+</body>
+</html>
