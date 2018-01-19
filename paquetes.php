2e37f6a0faf108c42138e069a9ae142193627795
jenifer0 <jenifer.vizcarra.0@gmail.com>
2018-01-19 01:03:40 -0500

    10

---
+++ paquetes.php
@@ -0,0 +1,29 @@
+<?PHP
+if ($_REQUEST['cod']==1){
+ echo "<strong> Cumpleaños : </strong>Disfruta de un cumpleaños diferente, en compañia de amigos y familiares, para una
+ fecha tan especial como esta tenemos para ti el Paquete de cumpleaños!! Al momento de realizar tu reserva indica que la
+ ocasion es tu cumpleaños y prepararemos una sorpresa especial para ti!!
+ <br> Grupos de 3 a 18 personas";
+ echo "<br><br><div align='center'><img src='img/cumple.jpg' border='0' width='50%'></div>"; }
+
+if ($_REQUEST['cod']==2)
+{
+ echo "<strong>Aniversario : </strong> Animate a celebrar cualquier tipo de aniversario con nostros! Al momento de realizar tu reserva indica que la
+ ocasion es tu cumpleaños y prepararemos una sorpresa especial para ti, ademas de un super descuento!!  <br> Grupos de 3 a 18 personas";
+ echo "<br><br><div align='center'><img src='img/aniv.jpeg' border='0' width='50%'></div>";
+}
+
+if ($_REQUEST['cod']==3)
+{
+ echo "<strong>Reencuentro : </strong> ¿Tienes un reecuentro de la promoción del colegio o de la universidad? Entonces ven a
+ celebrarlo con nostros, ofrecemos descuentos especiales por grupos de 25 personas a más.";
+ echo "<br><br><div align='center'><img src='img/reenc.jpg' border='0' width='50%'></div>";
+}
+
+if ($_REQUEST['cod']==4)
+{
+ echo "<strong> Empresas :</strong> ¿Vas a celebrar algun logro de tu empresa? No lo pienses más y ven a festejarlo en Picanteros
+ Arequipeños!, ofrecemos descuentos especiales en el area del bar por grupos de 10 personas como mínimo";
+ echo "<br><br><div align='center'><img src='img/empresa.jpg' border='0' width='50%'></div>";
+}
+?>
