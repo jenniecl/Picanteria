746af597997eadbed791376df562f63f1b5a9111
jenifer0 <jenifer.vizcarra.0@gmail.com>
2018-01-19 00:54:45 -0500

    4

---
+++ acercade.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<?php include('header.html');?>
+
+</head>
+<body>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Acerca de Nosotros</h1>
+			</div>
+
+			<div class="articulo" style = "float: left">
+				<article>
+					<p>Somos una empresa arequipeña, fundada en el año 1992 por la picantera Rocio Torres Avendaño
+						con el apoyo de toda su familia, nacimos con un pequeño local en el distrito tradicional de
+						Sachaca, el cual crecio con el tiempo, dando paso a instalaciones mas amplias y acogedoras
+						para todos nuestros clientes.
+						Mantenemos la tradicion Arequipeña de cocinar en hornos a leña y con batan, para asi conservar
+						el sabor de antaño; todos nuestros platos estan elaborados a base de los mejores insumos, la
+						mayoria de ellos cultivados en nuestra tierra, Arequipa, de manera natural. </p>
+					<br/>
+
+					<p id="listadoMenu">	MISIÓN</p><br/>
+						Ofrecer a nuestros clientes productos alimenticios de calidad, nutritivos y saludables; en un
+						ambiente agradable desarrollados por un equipo humano competente, comprometido en proporcionar
+						excelente servicio y satisfacción, generando desarrollo económico social al país y la empresa.</p>
+					<br/>
+					<p id="listadoMenu">	VISIÓN</p><br/>
+						Ser reconocidos como el mejor restaurante de prestigio, confiable en comida criolla,
+						 donde nuestro compromiso principal sea crear experiencias agradables al paladar de nuestros
+						  clientes. </p>
+				</article>
+
+			</div>
+			<div style = "float: left">
+				<?php include('sidebar.html');?>
+			</div>
+
+	</section>
+<?php include('footer.html');?>
+</body>
+</html>
